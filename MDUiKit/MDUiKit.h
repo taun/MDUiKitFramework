074505219ef4619e6f4e626fd385d56607cb2d38
Taun Chapman <tchapman@moedae.com>
2014-09-24 11:21:13 -0400

    Initial Commit

---
+++ MDUiKit/MDUiKit.h
@@ -0,0 +1,19 @@
+//
+//  MDUiKit.h
+//  MDUiKit
+//
+//  Created by Taun Chapman on 09/24/14.
+//  Copyright (c) 2014 MOEDAE LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDUiKit.
+FOUNDATION_EXPORT double MDUiKitVersionNumber;
+
+//! Project version string for MDUiKit.
+FOUNDATION_EXPORT const unsigned char MDUiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDUiKit/PublicHeader.h>
+
+

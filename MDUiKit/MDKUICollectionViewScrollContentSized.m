7d5c55ca6c445c9612a66b3936dc39428b6cb240
Taun Chapman <tchapman@moedae.com>
2014-11-18 15:13:18 -0500

    Fixed Head merge errors

--- MDUiKit/MDKUICollectionViewScrollContentSized.m
+++ MDUiKit/MDKUICollectionViewScrollContentSized.m
@@ -17,26 +17,7 @@
 
 -(void) awakeFromNib {
     [super awakeFromNib];
-    self.translatesAutoresizingMaskIntoConstraints = NO;
-    //    self.scrollEnabled = NO;
-    
-    [self configureConstraints];
 }
-<<<<<<< HEAD
--(void) configureConstraints {
-    CGFloat currentHeight = self.bounds.size.height;
-    _currentHeightConstraint = [NSLayoutConstraint constraintWithItem: self
-                                                            attribute: NSLayoutAttributeHeight
-                                                            relatedBy: NSLayoutRelationEqual
-                                                               toItem: nil
-                                                            attribute: NSLayoutAttributeNotAnAttribute
-                                                           multiplier: 1.0
-                                                             constant: currentHeight];
-    // Priority 999 seems necessary to stop autolayout constraint conflicts between desired height and UIView-Encapsulated-Layout-Height set by table class.
-    _currentHeightConstraint.priority = 999;
-    
-    [self addConstraint: _currentHeightConstraint];
-=======
 #pragma message "Can we just use setNeedsUpdateConstraints here then check at the delegate level? Reload cell if needsUpdate..."
 -(BOOL) nextItemWillWrapLine {
     BOOL willWrap = NO;
@@ -88,51 +69,20 @@
         // update superview
         [self.superview.superview setNeedsLayout];
     }
->>>>>>> DetachedHeadDial
 }
-//-(CGSize)sizeThatFits:(CGSize)size {
-//    CGSize bestSize = CGSizeMake(size.width, self.contentSize.height);
-//    return bestSize;
-//}
-//-(void)layoutSubviews {
-//    [super layoutSubviews];
-//    CGPoint origin = self.frame.origin;
-//    CGSize size = self.frame.size;
-//    
-//    if (self.contentSize.height != self.currentHeightConstraint.constant) {
-//        [self setNeedsUpdateConstraints];
-//    }
-//    if (self.contentSize.height != size.height) {
-//        // update superview
-////        self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, self.contentSize.height);
-//        // [self.superview setNeedsLayout];
-//    }
-//    [super layoutSubviews];
-//}
 
 -(void)updateConstraints {
     
-//    CGFloat newHeight = self.contentSize.height;
-//    
-//    BOOL test = newHeight > 140.0;
-//    
-//    if (newHeight == 0) {
-//        self.currentHeightConstraint.constant = 26.0;
-//    } else {
-//        self.currentHeightConstraint.constant = newHeight;
-//    }
+    CGFloat newHeight = self.contentSize.height;
+    
+    if (newHeight == 0) {
+        self.currentHeightConstraint.constant = 26.0;
+    } else {
+        self.currentHeightConstraint.constant = newHeight;
+    }
+    
     // change constraints above
     [super updateConstraints];
-//    [self.superview.superview.superview.superview setNeedsLayout];
 //    NSString* constraintsString = [self.constraints description];
 }
-//-(CGSize) intrinsicContentSize {
-//    CGFloat height = self.contentSize.height;
-//    CGSize newSize = CGSizeMake(UIViewNoIntrinsicMetric, height);
-//    return newSize;
-//}
-
-//-(void) viewFrameChanged:(UIView *)view {
-//    [self invalidateIntrinsicContentSize];
-//}
 @end

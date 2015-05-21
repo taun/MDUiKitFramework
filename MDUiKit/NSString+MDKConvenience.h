960d606ce9b1e54b1204193249766b8d1a21feb6
Taun Chapman <tchapman@moedae.com>
2015-05-21 14:17:46 -0400

    Re-integrated MDUiKitFramework as subproject of FractalScapes. Now working with Xcode 6...

--- MDUiKit/NSString+MDKConvenience.h
+++ MDUiKit/NSString+MDKConvenience.h
@@ -10,6 +10,15 @@
 
 @interface NSString (MDKConvenience)
 
++(NSString*) mdbStringByAppendingOrIncrementingCount: (NSString*)originalString;
+
+/*!
+ Take the receiving string and look up the file resource with the string name in the current bundle.
+ 
+ @return a property list object
+ */
+-(id) fromPListFileNameToObject;
+
 -(BOOL)isNonEmptyString;
 
 @end
--- MDUiKit/NSString+MDKConvenience.m
+++ MDUiKit/NSString+MDKConvenience.m
@@ -10,6 +10,46 @@
 
 @implementation NSString (MDKConvenience)
 
++(NSString*) mdbStringByAppendingOrIncrementingCount:(NSString *)originalString
+{
+    NSArray* substrings = [originalString componentsSeparatedByString: @" "];
+    NSString* lastComponent = [substrings lastObject];
+    NSInteger lastCopyInteger = [lastComponent integerValue]; // 0 if not a number so never use 0
+    NSMutableArray* newComponents = [substrings mutableCopy];
+    if (!lastCopyInteger) {
+        // equal 0 so last compoenent was not a number
+        [newComponents addObject: @" 1"];
+    } else {
+        //increment
+        [newComponents removeLastObject];
+        NSString* newCopyNumber = [NSString stringWithFormat: @" %ld", (long)++lastCopyInteger];
+        [newComponents addObject: newCopyNumber];
+    }
+    
+    NSString* newString = [newComponents componentsJoinedByString: @" "];
+    return newString;
+}
+
+/*
+ utility method to return the contents of a plist
+ */
+-(id) fromPListFileNameToObject {
+    NSError* error;
+    
+    NSString* plistPath = [[NSBundle mainBundle] pathForResource: self ofType: @"plist"];
+    NSData *plistXML = [[NSFileManager defaultManager] contentsAtPath: plistPath];
+    
+    id tempDefaults = [NSPropertyListSerialization propertyListWithData: plistXML
+                                                                options: 0
+                                                                 format: NULL
+                                                                  error: &error];
+    
+    if (!tempDefaults) {
+        NSLog(@"Error reading plist: %@", error);
+    }
+    return tempDefaults;
+}
+
 -(BOOL)isNonEmptyString
 {
     return (self && [self stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]].length > 0);
